2906b3c4d5ecb68964c5669635dc90ca7e4770ca
Frondorf, Niklas <niklas@frondorf.eu>
2024-07-03 23:15:34 +0200

    feat: added example for sources

---
+++ examples/sources.rs
@@ -0,0 +1,24 @@
+use openalex::types::{filter::Filter, sort::Sort, source::Source, APIEntity};
+
+pub fn main() {
+    let _source = Source::new("137773608").unwrap();
+    let _source_response = Source::get_samples(20, "1234").unwrap();
+    let _source_response = Source::filter(
+        Filter::builder()
+            .add_filter("has_issn", "true")
+            .and()
+            .add_filter("continent", "asia")
+            .build(),
+        1,
+        20,
+        Sort::builder().add_sort("cited_by_count", "desc").build(),
+    )
+    .unwrap();
+    let _source_response = Source::search(
+        "machine learning",
+        1,
+        20,
+        Sort::builder().add_sort("cited_by_count", "desc").build(),
+    )
+    .unwrap();
+}
