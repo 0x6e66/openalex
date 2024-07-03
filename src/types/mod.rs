9fb84a8f0bd0f858ceb7524dd6fcf56d6d39639f
Frondorf, Niklas <niklas@frondorf.eu>
2024-07-03 15:26:51 +0200

    refactor: replaced 'crate' with '$crate', because of clippy rule 'crate_in_macro_def'

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -9,29 +9,29 @@ pub mod source;
 macro_rules! impl_try_from_for_single_entity {
     ($entity:ty) => {
         impl TryFrom<reqwest::blocking::Response> for $entity {
-            type Error = crate::error::Error;
+            type Error = $crate::error::Error;
 
-            fn try_from(response: reqwest::blocking::Response) -> crate::prelude::Result<Self> {
+            fn try_from(response: reqwest::blocking::Response) -> $crate::prelude::Result<Self> {
                 match response.status() {
                     reqwest::StatusCode::OK => {
                         let res = response.json::<Self>();
                         match res {
                             Ok(entity) => Ok(entity),
-                            Err(e) => Err(crate::error::Error::Generic(format!(
+                            Err(e) => Err($crate::error::Error::Generic(format!(
                                 "Error deserializing Work object. Original Message: {}",
                                 e
                             ))),
                         }
                     }
                     reqwest::StatusCode::NOT_FOUND => {
-                        let oa_error = crate::error::OpenAlexError {
+                        let oa_error = $crate::error::OpenAlexError {
                             error: "Document not found".to_string(),
                             message: "The document with the specified id was not found. HTTP 404"
                                 .to_string(),
                         };
-                        Err(crate::error::Error::OpenAlex(oa_error))
+                        Err($crate::error::Error::OpenAlex(oa_error))
                     }
-                    _ => Err(crate::error::Error::Generic(format!(
+                    _ => Err($crate::error::Error::Generic(format!(
                         "Unknown Error. Response Code was {}",
                         response.status()
                     ))),
@@ -45,31 +45,31 @@ macro_rules! impl_try_from_for_single_entity {
 macro_rules! impl_try_from_for_entity_response {
     ($entity_response:ty) => {
         impl TryFrom<reqwest::blocking::Response> for $entity_response {
-            type Error = crate::error::Error;
+            type Error = $crate::error::Error;
 
-            fn try_from(response: reqwest::blocking::Response) -> crate::prelude::Result<Self> {
+            fn try_from(response: reqwest::blocking::Response) -> $crate::prelude::Result<Self> {
                 match response.status() {
                     reqwest::StatusCode::OK => {
                         let res = response.json::<Self>();
                         match res {
                             Ok(author_response) => Ok(author_response),
-                            Err(e) => Err(crate::error::Error::Generic(format!(
+                            Err(e) => Err($crate::error::Error::Generic(format!(
                                 "Error deserializing Work object. Original Message: {}",
                                 e
                             ))),
                         }
                     }
                     reqwest::StatusCode::FORBIDDEN => {
-                        let res = response.json::<crate::error::OpenAlexError>();
+                        let res = response.json::<$crate::error::OpenAlexError>();
                         match res {
-                            Ok(oa_error) => Err(crate::error::Error::OpenAlex(oa_error)),
-                            Err(e) => Err(crate::error::Error::Generic(format!(
+                            Ok(oa_error) => Err($crate::error::Error::OpenAlex(oa_error)),
+                            Err(e) => Err($crate::error::Error::Generic(format!(
                                 "Error deserializing OpenAlexError object. Original Message: {}",
                                 e
                             ))),
                         }
                     }
-                    _ => Err(crate::error::Error::Generic(format!(
+                    _ => Err($crate::error::Error::Generic(format!(
                         "Unknown Error. Response Code was {}",
                         response.status()
                     ))),
